29584186eebb7261c2c9fc78f0f4ec89fa9add03
YUAN YI <151559241+Leonildayyy@users.noreply.github.com>
2025-10-01 17:40:27 +0200

    Add files via upload

---
+++ Basic/credit_risk.py
@@ -0,0 +1,246 @@
+import polars as pl
+import numpy as np
+import pandas as pd
+import lightgbm as lgb
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score, precision_score, recall_score, f1_score
+
+from polars._utils.polars_version import get_polars_version
+
+###load the data
+dataPath='/Users/yuanyi/PycharmProjects/ML/risk_loan/home-credit-credit-risk-model-stability/'
+
+def set_table_dtypes(df: pl.DataFrame) -> pl.DataFrame:
+    #The function takes a Polars DataFrame (pl.DataFrame) as input. It returns a Polars DataFrame (pl.DataFrame) as output.
+
+    for col in df.columns:
+        # last letter of column name will help you determine the type
+        if col[-1] in ("P", "A"):
+            #columns ending with P indicate the days, with A indicate the amount
+            df = df.with_columns(pl.col(col).cast(pl.Float64).alias(col))
+            ##这一行就是把p和a的列，的datatype改成float，然后创建新的dataframe
+    return df
+
+def convert_strings(df: pd.DataFrame) -> pd.DataFrame:
+    #converts columns with object or string types to categorical data types.
+    for col in df.columns:
+        if df[col].dtype.name in ['object', 'string']:
+            df[col] = df[col].astype("string").astype('category')
+            ##变成categorical的variable后，想要得知有哪些unique的categories
+            current_categories = df[col].cat.categories
+            ##把不能分类的编入到unknown这一类里面
+            new_categories = current_categories.to_list() + ["Unknown"]
+            #加入unknown这一个category到原来的这一列
+            new_dtype = pd.CategoricalDtype(categories=new_categories, ordered=True)
+            df[col] = df[col].astype(new_dtype)
+    return df
+
+##training data set
+train_basetable = pl.read_csv(dataPath + "csv_files/train/train_base.csv")
+
+train_static = pl.concat(
+    [
+        pl.read_csv(dataPath + "csv_files/train/train_static_0_0.csv").pipe(set_table_dtypes),
+        pl.read_csv(dataPath + "csv_files/train/train_static_0_1.csv").pipe(set_table_dtypes),
+    ],
+    how="vertical_relaxed",
+)
+
+train_static_cb = pl.read_csv(dataPath + "csv_files/train/train_static_cb_0.csv").pipe(set_table_dtypes)
+train_person_1 = pl.read_csv(dataPath + "csv_files/train/train_person_1.csv").pipe(set_table_dtypes)
+train_credit_bureau_b_2 = pl.read_csv(dataPath + "csv_files/train/train_credit_bureau_b_2.csv").pipe(set_table_dtypes)
+
+##testing data set
+test_basetable = pl.read_csv(dataPath + "csv_files/test/test_base.csv")
+
+test_static = pl.concat(
+    [
+        pl.read_csv(dataPath + "csv_files/test/test_static_0_0.csv").pipe(set_table_dtypes),
+        pl.read_csv(dataPath + "csv_files/test/test_static_0_1.csv").pipe(set_table_dtypes),
+        pl.read_csv(dataPath + "csv_files/test/test_static_0_2.csv").pipe(set_table_dtypes),
+    ],
+    how="vertical_relaxed",
+)
+test_static_cb = pl.read_csv(dataPath + "csv_files/test/test_static_cb_0.csv").pipe(set_table_dtypes)
+test_person_1 = pl.read_csv(dataPath + "csv_files/test/test_person_1.csv").pipe(set_table_dtypes)
+test_credit_bureau_b_2 = pl.read_csv(dataPath + "csv_files/test/test_credit_bureau_b_2.csv").pipe(set_table_dtypes)
+
+###Feature engineering
+
+#for tables with depth>0, we may need to employ aggregation functions that will condense the historical records associated with each case_id into a single feature.
+#train_person_1 and train_credit_bureau_b_2
+
+train_person_1.select(pl.col("incometype_1044T").value_counts())
+
+train_person_1_feats_1 = train_person_1.group_by("case_id").agg(
+    pl.col("mainoccupationinc_384A").max().alias("mainoccupationinc_384A_max"),
+    (pl.col("incometype_1044T") == "SELFEMPLOYED").max().alias("mainoccupationinc_384A_any_selfemployed")
+) ##two variables: amount of income, type of income 显示每个caseid下最高的income，然后几条记录中如果有自雇的，就返回true
+
+print(train_person_1_feats_1)
+
+# Here num_group1=0 has special meaning, it is the person who applied for the loan.
+train_person_1_feats_2 = train_person_1.select(["case_id", "num_group1", "housetype_905L"]).filter(
+    pl.col("num_group1") == 0
+).drop("num_group1").rename({"housetype_905L": "person_housetype"})
+
+print(train_person_1_feats_2.select(pl.col("person_housetype").value_counts()))
+
+# Here we have num_goup1 and num_group2, so we need to aggregate again.
+train_credit_bureau_b_2_feats = train_credit_bureau_b_2.group_by("case_id").agg(
+    pl.col("pmts_pmtsoverdue_635A").max().alias("pmts_pmtsoverdue_635A_max"),
+    (pl.col("pmts_dpdvalue_108P") > 31).max().alias("pmts_dpdvalue_108P_over31")
+)
+## The maximum value of pmts_pmtsoverdue_635A for each case_id.
+# Whether there is any instance where pmts_dpdvalue_108P exceeds 31 days for each case_id.
+
+# We will process in these examples only A-type and M-type columns, so we need to select them.
+selected_static_cols = []
+for col in train_static.columns:
+    if col[-1] in ("A", "M"):
+        selected_static_cols.append(col)
+print(selected_static_cols)
+
+selected_static_cb_cols = []
+for col in train_static_cb.columns:
+    if col[-1] in ("A", "M"):
+        selected_static_cb_cols.append(col)
+print(selected_static_cb_cols)
+
+# Join all tables together.
+data = train_basetable.join(
+    train_static.select(["case_id"]+selected_static_cols), how="left", on="case_id"
+).join(
+    train_static_cb.select(["case_id"]+selected_static_cb_cols), how="left", on="case_id"
+).join(
+    train_person_1_feats_1, how="left", on="case_id"
+).join(
+    train_person_1_feats_2, how="left", on="case_id"
+).join(
+    train_credit_bureau_b_2_feats, how="left", on="case_id"
+)
+
+data.head(5)
+print(data.select(pl.col("target").value_counts()))
+
+##same steps for testing data
+test_person_1_feats_1 = test_person_1.group_by("case_id").agg(
+    pl.col("mainoccupationinc_384A").max().alias("mainoccupationinc_384A_max"),
+    (pl.col("incometype_1044T") == "SELFEMPLOYED").max().alias("mainoccupationinc_384A_any_selfemployed")
+)
+
+test_person_1_feats_2 = test_person_1.select(["case_id", "num_group1", "housetype_905L"]).filter(
+    pl.col("num_group1") == 0
+).drop("num_group1").rename({"housetype_905L": "person_housetype"})
+
+test_credit_bureau_b_2_feats = test_credit_bureau_b_2.group_by("case_id").agg(
+    pl.col("pmts_pmtsoverdue_635A").max().alias("pmts_pmtsoverdue_635A_max"),
+    (pl.col("pmts_dpdvalue_108P") > 31).max().alias("pmts_dpdvalue_108P_over31")
+)
+
+data_submission = test_basetable.join(
+    test_static.select(["case_id"]+selected_static_cols), how="left", on="case_id"
+).join(
+    test_static_cb.select(["case_id"]+selected_static_cb_cols), how="left", on="case_id"
+).join(
+    test_person_1_feats_1, how="left", on="case_id"
+).join(
+    test_person_1_feats_2, how="left", on="case_id"
+).join(
+    test_credit_bureau_b_2_feats, how="left", on="case_id"
+)
+
+# splitting the dataset into training, validation, and testing sets
+case_ids = data["case_id"].unique().shuffle(seed=1)
+
+print(len(data["case_id"]))
+print(len(case_ids))
+
+##case_ids_train,case_ids_valid,case_ids_test
+#先分training:0.6, 剩下0.4做test，再把test分两份，validation:0.2, testing:0.2
+case_ids_train, case_ids_test = train_test_split(case_ids, train_size=0.6, random_state=1)
+case_ids_valid, case_ids_test = train_test_split(case_ids_test, train_size=0.5, random_state=1)
+
+#columns ending with A or M
+data.columns
+
+cols_pred = []
+for col in data.columns:
+    if col[-1] in ("A", "M") and col != "WEEK_NUM":  # Check if the last character is 'A' or 'M'
+        cols_pred.append(col)
+print(cols_pred) ##the features to build the model
+
+#diving it into base,X and y
+def from_polars_to_pandas(case_ids: pl.DataFrame) -> pl.DataFrame:
+    return (
+        data.filter(pl.col("case_id").is_in(case_ids))[["case_id", "WEEK_NUM", "target"]].to_pandas(),
+        data.filter(pl.col("case_id").is_in(case_ids))[cols_pred].to_pandas(),
+        data.filter(pl.col("case_id").is_in(case_ids))["target"].to_pandas()
+    )
+
+#apply function from_polars_to_pandas to case_ids_train, returning base_train, X_train, y_train
+base_train, X_train, y_train = from_polars_to_pandas(case_ids_train)
+base_valid, X_valid, y_valid = from_polars_to_pandas(case_ids_valid)
+base_test, X_test, y_test = from_polars_to_pandas(case_ids_test)
+
+base_train.head()
+
+
+for df in [X_train, X_valid, X_test]:
+    df = convert_strings(df)
+
+print(f"Train: {X_train.shape}")
+print(f"Valid: {X_valid.shape}")
+print(f"Test: {X_test.shape}")
+
+#Model Training LightGBM
+
+#lgb.Dataset: This creates a dataset object specifically for LightGBM, which is optimized for faster training.
+lgb_train = lgb.Dataset(X_train, label=y_train)
+lgb_valid = lgb.Dataset(X_valid, label=y_valid, reference=lgb_train)
+
+params = {
+    "boosting_type": "gbdt",
+    "objective": "binary",
+    "metric": "auc",
+    "max_depth": 3,
+    "num_leaves": 31,
+    "learning_rate": 0.05,
+    "feature_fraction": 0.9,
+    "bagging_fraction": 0.8,
+    "bagging_freq": 5,
+    "n_estimators": 1000,
+    "verbose": -1,
+    "scale_pos_weight": 1478665 / 47994, # adjust for class imbalance
+}
+
+gbm = lgb.train(
+    params,
+    lgb_train,
+    valid_sets=lgb_valid,
+    callbacks=[lgb.log_evaluation(50), lgb.early_stopping(10)]
+)
+
+base_train.head()
+
+#This loop iterates over three pairs of datasets: base_train with X_train, base_valid with X_valid, and base_test with X_test.
+for base, X in [(base_train, X_train), (base_valid, X_valid), (base_test, X_test)]:
+    y_pred = gbm.predict(X, num_iteration=gbm.best_iteration)
+    base["score"] = y_pred  #The predicted probabilities (y_pred) are then added as a new column called score in base_train
+
+print(f'The AUC score on the train set is: {roc_auc_score(base_train["target"], base_train["score"])}')
+print(f'The AUC score on the valid set is: {roc_auc_score(base_valid["target"], base_valid["score"])}')
+print(f'The AUC score on the test set is: {roc_auc_score(base_test["target"], base_test["score"])}')
+
+def evaluate_cutoffs(y_true, y_pred_proba, cutoffs=[0.3, 0.5, 0.7]):
+    for c in cutoffs:
+        y_pred = (y_pred_proba >= c).astype(int)
+        precision = precision_score(y_true, y_pred)
+        recall = recall_score(y_true, y_pred)
+        f1 = f1_score(y_true, y_pred)
+        print(f"Cutoff = {c}")
+        print(f"  Precision: {precision:.4f}")
+        print(f"  Recall:    {recall:.4f}")
+        print(f"  F1-score:  {f1:.4f}\n")
+
+evaluate_cutoffs(base_test["target"], base_test["score"])
